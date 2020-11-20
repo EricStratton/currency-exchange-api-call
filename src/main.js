6cf3646ed54e01bc4acf37cbdff5156b5f42ae7c
Eric Stratton <strattonericj@gmail.com>
2020-11-20 11:04:45 -0800

    fix import statement for API request, request now working properly in app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from '.src/services/currency-service.js'
-import CurrencyExchange from '.src/js/currency-exchange.js'
+import CurrencyService from './services/currency-service.js';
+
 
 
 // User Interface Logic //
 
+function calcExchange(response, USD, otherCountry) {
+  if (response.conversion_rates) {
+    $('#printExchange').text(USD*response.conversion_rates[`${otherCountry}`]);
+  }
+}
+
+$(document).ready(function() {
+  $('form#userInput').submit(function(event) {
+    event.preventDefault();
+
+    const otherCountry = $('#otherCountry').val();
+    const USD = $('#usd').val();
+    console.log(USD);
+    console.log(otherCountry);
+
+    CurrencyService.getCurrency()
+      .then(function(response) {
+        calcExchange(response, USD, otherCountry);
+        console.log(response);
+      });
+
+  });
+}); 
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -3,12 +3,12 @@ export default class CurrencyService {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
-          throw Error(response.result)
+          throw Error(response.result);
         }
         return response.json();
       })
       .catch(function(error) {
         return error;
-      })
+      });
   }
 }
