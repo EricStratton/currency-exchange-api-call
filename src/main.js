bcd8dde7e5a50ca4eefbe84e18d2b0c0d8e4ede3
Eric Stratton <strattonericj@gmail.com>
2020-11-20 09:45:59 -0800

    add basic html body, add import statements to main.js, remove parameter from currency-service

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from '.src/services/currency-service.js'
-
-
-
-
-
-
+import CurrencyExchange from '.src/js/currency-exchange.js'
 
 
 // User Interface Logic //
+
--- src/services/currecy-service.js
+++ src/services/currecy-service.js
@@ -1,5 +1,5 @@
 export default class CurrencyService {
-  static getCurrency(country) {
+  static getCurrency(USD) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
   }
 }
