3e93853ee0a70fe76f49f5c4d31593c1ce8ff5a3
Eric Stratton <strattonericj@gmail.com>
2020-11-20 13:21:16 -0800

    include error message if currency selected is not available in API response

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import './css/styles.css';
 import CurrencyService from './services/currency-service.js';
 
 
-
 // Business Logic //
 
 function calcExchange(response, USD, otherCountry) {
   if (response.conversion_rates) {
+    if (isNaN(response.conversion_rates[`${otherCountry}`])) {
+      return $('#printErrors').text("We are sorry. The currency you selected is not currently available for conversion.")
+    }
     $('#printExchange').text((USD*response.conversion_rates[`${otherCountry}`]) );
   } else {
     $('#printErrors').text(`Error: ${response.message}`);
@@ -22,6 +24,7 @@ $(document).ready(function() {
   $('form#userInput').submit(function(event) {
     event.preventDefault();
 
+    $('#printErrors').empty();
     const otherCountry = $('#otherCountry').val();
     const USD = $('#usd').val();
     
